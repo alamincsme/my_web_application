bf7d0c082e322162e57eddb5a684434e9a47d770
alamincsme <alamincsme@gmail.com>
2024-01-09 19:28:49 +0600

    Implementation all for user

--- src/main/java/com/alamincse/main/user/UserRepository.java
+++ src/main/java/com/alamincse/main/user/UserRepository.java
@@ -1,5 +1,9 @@
 package com.alamincse.main.user;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public Long countById(Integer id);
 }
