6680cb7a045cb6e7a6427390bf7b0596ef746937
alamincsme <alamincsme@gmail.com>
2024-02-21 14:15:55 +0600

    updated user repository

---
+++ src/main/java/com/alamincse/main/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alamincse.main.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Long countById(Integer id);
+}
